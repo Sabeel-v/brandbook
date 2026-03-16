e67c0de4b80714c2306efe9ad327664c8d67a263
sabeel <sabeelv825@gmail.com>
2026-03-16 12:51:40 +0530

    deploy to render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 CORS_ALLOW_ALL_ORIGINS = True
